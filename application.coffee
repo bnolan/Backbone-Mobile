ed0972fbd5b7a438f6c477850c2442f5440d8da7
Ben Nolan <ben@nolanconsul.com>
2010-11-24 09:23:45 +1300

    * Commit

---
+++ application.coffee
@@ -0,0 +1,248 @@
+
+#
+# Some helper methods
+#
+
+app =
+  activePage: ->
+    $(".ui-page-active")
+    
+  reapplyStyles: (el) ->
+    el.find('ul[data-role]').listview();
+    el.find('div[data-role="fieldcontain"]').fieldcontain();
+    el.find('button[data-role="button"]').button();
+    el.find('input,textarea').textinput();
+    el.page()
+    
+  redirectTo: (page) ->
+    $.mobile.changePage page
+    
+
+#
+# Venue class
+#
+
+class Venue extends Backbone.Model
+  getName: ->
+    @get('name')
+    
+  getAddress: ->
+    [@get('address'), @get('city'), @get('state')].join ", "
+    
+  getImageUrl: ->
+    @get('photo_url')
+    
+  getLatitude: ->
+    @get('geolat')
+
+  getLongitude: ->
+    @get('geolong')
+
+  getMapUrl: (width, height) ->
+    width ||= 300
+    height ||= 220
+    
+    "http://maps.google.com/maps/api/staticmap?center=#{@getLatitude()},#{@getLongitude()}&zoom=14&size=#{width}x#{height}&maptype=terrain&markers=color:red|#{@getLatitude()},#{@getLongitude()}&sensor=false"
+    
+#
+# Venue Collection
+#
+
+class VenueCollection extends Backbone.Collection
+  model : Venue
+  
+  constructor: ->
+    @refresh($FOURSQUARE_JSON)
+  
+this.Venues = new VenueCollection
+
+#
+# Edit Venue View
+#
+
+class EditVenueView extends Backbone.View
+  constructor: ->
+    super
+    
+    # Get the active page from jquery mobile. We need to keep track of what this
+    # dom element is so that we can refresh the page when the page is no longer active.
+    @el = app.activePage()
+    
+    @template = _.template('''
+    <form action="#venue-<%= venue.cid %>-update" method="post">
+
+      <div data-role="fieldcontain">
+        <label>Name</label>
+        <input type="text" value="<%= venue.getName() %>" name="name" />
+      </div>
+      
+      <div data-role="fieldcontain">
+        <label>Address</label>
+        <input type="text" value="<%= venue.get('address') %>" name="address" />
+      </div>
+      
+      <div data-role="fieldcontain">
+        <label>City</label>
+        <input type="text" value="<%= venue.get('city') %>" name="city" />
+      </div>
+      
+      <div data-role="fieldcontain">
+        <label>State</label>
+        <input type="text" value="<%= venue.get('state') %>" name="state" />
+      </div>
+      
+      <button type="submit" data-role="button">Save</button>
+    </form>
+    ''')
+    
+    # Watch for changes to the model and redraw the view
+    @model.bind 'change', @render
+    
+    # Draw the view
+    @render()
+    
+  events : {
+    "submit form" : "onSubmit"
+  }
+
+  onSubmit: (e) ->
+    @model.set {
+      name : $("input[name='name']").val(),
+      address : $("input[name='address']").val(),
+      city : $("input[name='city']").val(),
+      state : $("input[name='state']").val()
+    }
+    
+    @model.trigger('change')
+
+    app.redirectTo "#venues-#{@model.cid}"
+    
+    e.preventDefault()
+    e.stopPropagation()
+
+  render: =>
+    # Set the name of the page
+    @el.find('h1').text("Editing #{@model.getName()}")
+    
+    # Render the content
+    @el.find('.ui-content').html(@template({venue : @model}))
+
+    # A hacky way of reapplying the jquery mobile styles
+    app.reapplyStyles(@el)
+
+    # Delegate from the events hash
+    @delegateEvents()
+
+#
+# Show Venue View
+#
+
+class ShowVenueView extends Backbone.View
+  constructor: ->
+    super
+    
+    # Get the active page from jquery mobile. We need to keep track of what this
+    # dom element is so that we can refresh the page when the page is no longer active.
+    @el = app.activePage()
+    
+    @template = _.template('''
+      <div>
+        
+        <p>
+          <img style="width: 100%" src="<%= venue.getMapUrl() %>" />
+        </p>
+        
+        <address>
+          <%= venue.getAddress() %>
+        </address>
+      
+        <ul data-role="listview" data-inset="true">
+          <li data-role="list-divider">Actions</li>
+          <li><a rel="external" href="openmap:q=<%= encodeURIComponent(venue.getAddress) %>">Open Map</li>
+          <li><a href="#venues-<%= venue.cid %>-edit">Edit</a></li>
+        </ul>
+      </div>
+    ''')
+    
+    # Watch for changes to the model and redraw the view
+    @model.bind 'change', @render
+    
+    # Draw the view
+    @render()
+    
+  render: =>
+    # Set the name of the page
+    @el.find('h1').text(@model.getName())
+    
+    # Render the content
+    @el.find('.ui-content').html(@template({venue : @model}))
+
+    # A hacky way of reapplying the jquery mobile styles
+    app.reapplyStyles(@el)
+  
+#
+# Home View
+#
+  
+class HomeView extends Backbone.View
+  constructor: ->
+    super
+    
+    @el = app.activePage()
+    
+    @template = _.template('''
+      <div>
+      
+      <ul data-role="listview" data-theme="c" data-filter="true">
+        <% venues.each(function(venue){ %>
+          <li><a href="#venues-<%= venue.cid %>"><%= venue.getName() %></a></li>
+        <% }); %>
+      </ul>
+      
+      </div>
+    ''')
+    
+    @render()
+    
+  render: =>
+    # Render the content
+    @el.find('.ui-content').html(@template({venues : Venues}))
+
+    # A hacky way of reapplying the jquery mobile styles
+    app.reapplyStyles(@el)  
+    
+    
+#
+# Our only controller
+#
+
+class HomeController extends Backbone.Controller
+  routes :
+    "venues-:cid-edit" : "edit"
+    "venues-:cid" : "show"
+    "home"  : "home"
+
+  constructor: ->
+    super
+    @_views = {}
+
+  home : ->
+    @_views['home'] ||= new HomeView
+    
+  show: (cid) ->
+    @_views["venues-#{cid}"] ||= new ShowVenueView { model : Venues.getByCid(cid) }
+
+  edit: (cid) ->
+    @_views["venues-#{cid}-edit"] ||= new EditVenueView { model : Venues.getByCid(cid) }
+
+app.homeController = new HomeController()
+
+#
+# Start the app
+#  
+
+$(document).ready ->
+  Backbone.history.start()
+  app.homeController.home()
+  
+@app = app
---
+++ application.js
@@ -0,0 +1,178 @@
+(function() {
+  var EditVenueView, HomeController, HomeView, ShowVenueView, Venue, VenueCollection, app;
+  var __extends = function(child, parent) {
+    function ctor() { this.constructor = child; }
+    ctor.prototype = parent.prototype;
+    child.prototype = new ctor;
+    if (typeof parent.extended === "function") parent.extended(child);
+    child.__super__ = parent.prototype;
+  };
+  app = {
+    activePage: function() {
+      return $(".ui-page-active");
+    },
+    reapplyStyles: function(el) {
+      el.find('ul[data-role]').listview();
+      el.find('div[data-role="fieldcontain"]').fieldcontain();
+      el.find('button[data-role="button"]').button();
+      el.find('input,textarea').textinput();
+      return el.page();
+    },
+    redirectTo: function(page) {
+      return $.mobile.changePage(page);
+    }
+  };
+  Venue = (function() {
+    function Venue() {
+      return Backbone.Model.apply(this, arguments);
+    }
+    return Venue;
+  })();
+  __extends(Venue, Backbone.Model);
+  Venue.prototype.getName = function() {
+    return this.get('name');
+  };
+  Venue.prototype.getAddress = function() {
+    return [this.get('address'), this.get('city'), this.get('state')].join(", ");
+  };
+  Venue.prototype.getImageUrl = function() {
+    return this.get('photo_url');
+  };
+  Venue.prototype.getLatitude = function() {
+    return this.get('geolat');
+  };
+  Venue.prototype.getLongitude = function() {
+    return this.get('geolong');
+  };
+  Venue.prototype.getMapUrl = function(width, height) {
+    width || (width = 300);
+    height || (height = 220);
+    return "http://maps.google.com/maps/api/staticmap?center=" + (this.getLatitude()) + "," + (this.getLongitude()) + "&zoom=14&size=" + width + "x" + height + "&maptype=terrain&markers=color:red|" + (this.getLatitude()) + "," + (this.getLongitude()) + "&sensor=false";
+  };
+  VenueCollection = (function() {
+    function VenueCollection() {
+      this.refresh($FOURSQUARE_JSON);
+      return this;
+    }
+    return VenueCollection;
+  })();
+  __extends(VenueCollection, Backbone.Collection);
+  VenueCollection.prototype.model = Venue;
+  this.Venues = new VenueCollection;
+  EditVenueView = (function() {
+    function EditVenueView() {
+      var _this;
+      _this = this;
+      this.render = function() { return EditVenueView.prototype.render.apply(_this, arguments); };
+      EditVenueView.__super__.constructor.apply(this, arguments);
+      this.el = app.activePage();
+      this.template = _.template('<form action="#venue-<%= venue.cid %>-update" method="post">\n\n  <div data-role="fieldcontain">\n    <label>Name</label>\n    <input type="text" value="<%= venue.getName() %>" name="name" />\n  </div>\n  \n  <div data-role="fieldcontain">\n    <label>Address</label>\n    <input type="text" value="<%= venue.get(\'address\') %>" name="address" />\n  </div>\n  \n  <div data-role="fieldcontain">\n    <label>City</label>\n    <input type="text" value="<%= venue.get(\'city\') %>" name="city" />\n  </div>\n  \n  <div data-role="fieldcontain">\n    <label>State</label>\n    <input type="text" value="<%= venue.get(\'state\') %>" name="state" />\n  </div>\n  \n  <button type="submit" data-role="button">Save</button>\n</form>');
+      this.model.bind('change', this.render);
+      this.render();
+      return this;
+    }
+    return EditVenueView;
+  })();
+  __extends(EditVenueView, Backbone.View);
+  EditVenueView.prototype.events = {
+    "submit form": "onSubmit"
+  };
+  EditVenueView.prototype.onSubmit = function(e) {
+    this.model.set({
+      name: $("input[name='name']").val(),
+      address: $("input[name='address']").val(),
+      city: $("input[name='city']").val(),
+      state: $("input[name='state']").val()
+    });
+    this.model.trigger('change');
+    app.redirectTo("#venues-" + this.model.cid);
+    e.preventDefault();
+    return e.stopPropagation();
+  };
+  EditVenueView.prototype.render = function() {
+    this.el.find('h1').text("Editing " + (this.model.getName()));
+    this.el.find('.ui-content').html(this.template({
+      venue: this.model
+    }));
+    app.reapplyStyles(this.el);
+    return this.delegateEvents();
+  };
+  ShowVenueView = (function() {
+    function ShowVenueView() {
+      var _this;
+      _this = this;
+      this.render = function() { return ShowVenueView.prototype.render.apply(_this, arguments); };
+      ShowVenueView.__super__.constructor.apply(this, arguments);
+      this.el = app.activePage();
+      this.template = _.template('<div>\n  \n  <p>\n    <img style="width: 100%" src="<%= venue.getMapUrl() %>" />\n  </p>\n  \n  <address>\n    <%= venue.getAddress() %>\n  </address>\n\n  <ul data-role="listview" data-inset="true">\n    <li data-role="list-divider">Actions</li>\n    <li><a rel="external" href="openmap:q=<%= encodeURIComponent(venue.getAddress) %>">Open Map</li>\n    <li><a href="#venues-<%= venue.cid %>-edit">Edit</a></li>\n  </ul>\n</div>');
+      this.model.bind('change', this.render);
+      this.render();
+      return this;
+    }
+    return ShowVenueView;
+  })();
+  __extends(ShowVenueView, Backbone.View);
+  ShowVenueView.prototype.render = function() {
+    this.el.find('h1').text(this.model.getName());
+    this.el.find('.ui-content').html(this.template({
+      venue: this.model
+    }));
+    return app.reapplyStyles(this.el);
+  };
+  HomeView = (function() {
+    function HomeView() {
+      var _this;
+      _this = this;
+      this.render = function() { return HomeView.prototype.render.apply(_this, arguments); };
+      HomeView.__super__.constructor.apply(this, arguments);
+      this.el = app.activePage();
+      this.template = _.template('<div>\n\n<ul data-role="listview" data-theme="c" data-filter="true">\n  <% venues.each(function(venue){ %>\n    <li><a href="#venues-<%= venue.cid %>"><%= venue.getName() %></a></li>\n  <% }); %>\n</ul>\n\n</div>');
+      this.render();
+      return this;
+    }
+    return HomeView;
+  })();
+  __extends(HomeView, Backbone.View);
+  HomeView.prototype.render = function() {
+    this.el.find('.ui-content').html(this.template({
+      venues: Venues
+    }));
+    return app.reapplyStyles(this.el);
+  };
+  HomeController = (function() {
+    function HomeController() {
+      HomeController.__super__.constructor.apply(this, arguments);
+      this._views = {};
+      return this;
+    }
+    return HomeController;
+  })();
+  __extends(HomeController, Backbone.Controller);
+  HomeController.prototype.routes = {
+    "venues-:cid-edit": "edit",
+    "venues-:cid": "show",
+    "home": "home"
+  };
+  HomeController.prototype.home = function() {
+    var _base;
+    return (_base = this._views)['home'] || (_base['home'] = new HomeView);
+  };
+  HomeController.prototype.show = function(cid) {
+    var _base, _name;
+    return (_base = this._views)[_name = "venues-" + cid] || (_base[_name] = new ShowVenueView({
+      model: Venues.getByCid(cid)
+    }));
+  };
+  HomeController.prototype.edit = function(cid) {
+    var _base, _name;
+    return (_base = this._views)[_name = "venues-" + cid + "-edit"] || (_base[_name] = new EditVenueView({
+      model: Venues.getByCid(cid)
+    }));
+  };
+  app.homeController = new HomeController();
+  $(document).ready(function() {
+    Backbone.history.start();
+    return app.homeController.home();
+  });
+  this.app = app;
+}).call(this);
